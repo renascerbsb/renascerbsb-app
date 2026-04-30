dfa7722431ff1358c6f655f3504459f20e425278
Liciane Morato <liciane.venturin@agro.gov.br>
2026-04-30 16:27:53 -0300

    melhorias

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -7,4 +7,36 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
-export class Navbar {}
+export class Navbar {
+
+  isLight = false;
+
+  ngOnInit() {
+    const savedTheme = localStorage.getItem('theme');
+
+    if (savedTheme === 'light') {
+      this.setLightTheme();
+      this.isLight = true;
+    }
+  }
+
+  toggleTheme() {
+    this.isLight = !this.isLight;
+
+    if (this.isLight) {
+      this.setLightTheme();
+    } else {
+      this.setDarkTheme();
+    }
+  }
+
+  setLightTheme() {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+  }
+
+  setDarkTheme() {
+    document.documentElement.removeAttribute('data-theme');
+    localStorage.setItem('theme', 'dark');
+  }
+}
